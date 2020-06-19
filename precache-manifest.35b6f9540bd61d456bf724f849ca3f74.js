b79ae1bdb30b94f16b02daa935f1d72559c9fd4b
amaboubakr85 <a.m.aboubakr85@gmail.com>
2020-06-19 22:52:40 +0300

    Updates

---
+++ precache-manifest.35b6f9540bd61d456bf724f849ca3f74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41e557d03ade221240ad24232149c885",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "67d6a44feb517baf7bc7",
+    "url": "/monsters-rolodex/static/css/main.4b1df0b7.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67d6a44feb517baf7bc7",
+    "url": "/monsters-rolodex/static/js/main.c7649106.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
